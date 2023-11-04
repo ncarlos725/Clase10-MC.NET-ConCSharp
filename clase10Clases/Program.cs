fd49b59b4d29da74ff21b29b487f2b052b4887c1
ncarlos725 <ncarlos725@gmail.com>
2023-11-04 10:51:03 -0300

    Clases parte 1

---
+++ clase10Clases/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase10Clases
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>();
+
+            Student estudianteJorge = new Student("Jorge","Perez","Calle123");
+     
+            student.Add(estudianteJorge);
+
+            Student estudianteFrancis = new Student("Fran","Quiroga","SiempreViva");
+          
+
+            
+
+            PrintStudentNames(student);
+
+           
+        }
+
+        //funciones
+        private static void PrintStudentNames(List<Student> students) 
+        {
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
---
+++ clase10Clases/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase10Clases
+{
+    class Student
+    {
+        private static int Counter;
+
+        public int Id 
+        { 
+            get; private set; 
+        } 
+       public string Name
+        {
+            get; private set;
+        }
+
+        public string LastName
+        {
+            get; private set;
+
+        }
+        
+        public string Address
+        {
+            get; private set;
+        }
+        // Ejemplo de como llamar un constructor en otro constructor
+        public Student(String name, string lastName) : this(name,lastName, "")
+        { 
+        }
+
+        // Constructor
+        public Student(string name, string lastName, string addres)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                
+            }
+            if (string.IsNullOrEmpty(LastName)) 
+            {
+
+            }
+    
+
+            Counter++;
+            this.Id = Counter;
+
+            this.Name = name;
+            this.LastName = lastName;
+            this.Address = addres;
+        }
+        public void Matricular()
+        {
+            // hace algo
+        }
+
+    
+    }// cllas student
+
+}// namespace
